1a9b8e1c9821920de67a519410e07514b690b198
shraddha salvi <shraddhasalvi272@gmail.com>
2024-12-08 17:16:02 +0530

    created database models

---
+++ backend/models/blog.model.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+import { User } from "./user.model";
+
+const blogSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    blogImange:{
+        public_id:{
+            type:String,
+            required:true,
+        },
+        url:{
+            type:String,
+            required:true,
+        }
+    },
+    category:{
+        type:String,
+        required:true,
+
+    },
+    about:{
+        type:String,
+        required:true,
+        minlength:[200,"Should contain atleast 200 characters"]
+
+    },
+    adminname:{
+        type:String,
+        required:true,
+
+    },
+    adminPhoto:{
+        type:String
+    },
+    createdBy:{
+        type:mongoose.Schema.ObjectId,
+       ref:"User",
+
+    }
+
+
+});
+
+export const Blog = mongoose.model("Blog",blogSchema);
