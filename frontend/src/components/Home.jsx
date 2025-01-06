23ff0abe025feb942833be893f2c3e088c7645fc
shraddha salvi <shraddhasalvi272@gmail.com>
2025-01-06 12:44:08 +0530

    done with basic frontend

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Hero from "../Home/Hero";
+import Trending from "../Home/Trending";
+import Devotional from "../Home/Devotional";
+import Creator from "../Home/Creator";
 
-const Home =()=>{
-    return(
-        <div>Home</div>
-    )
-
+function Home() {
+  return (
+    <div>
+      <Hero />
+      <Trending />
+      <Devotional />
+      <Creator />
+    </div>
+  );
 }
 
 export default Home;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,36 @@
-import React from "react";
-const Dashboard = ()=>{
-    return {
-        
-    }
+import React, { useState } from "react";
+import { useAuth } from "../context/AuthProvider";
+import Sidebar from "../dashboard/SideBar";
+import MyProfile from "../dashboard/MyProfile";
+import MyBlogs from "../dashboard/MyBlog";
+import CreateBlog from "../dashboard/CreateBlog";
+import UpdateBlog from "../dashboard/UpdateBlog";
+import { Navigate } from "react-router-dom";
+function Dashboard() {
+  const { profile, isAuthenticated } = useAuth();
+  const [component, setComponent] = useState("My Blogs");
+  console.log(profile);
+  console.log(isAuthenticated);
 
+  if (!isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+  return (
+    <div>
+      <div>
+        <Sidebar component={component} setComponent={setComponent} />
+        {component === "My Profile" ? (
+          <MyProfile />
+        ) : component === "Create Blog" ? (
+          <CreateBlog />
+        ) : component === "Update Blog" ? (
+          <UpdateBlog />
+        ) : (
+          <MyBlogs />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
